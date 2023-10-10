fb31770908f5b53db46c11018437775534df0714
leokingue <ebangkingue@icloud.com>
2023-10-11 00:16:10 +0200

    update the task0

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -3,6 +3,8 @@ import asyncio
 import random
 '''
 an asynchronous coroutine that takes in an integer argument
+(max_delay, with a default value of 10) named wait_random
+ that waits for a random delay between 0 and max_delay
 '''
 
 
