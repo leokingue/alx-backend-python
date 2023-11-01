9d9ff7045c492fee0d3c8d3148af2e6051320c81
leokingue <ebangkingue@icloud.com>
2023-11-01 13:22:18 +0100

    Add the test_access_nested_map_exceptioon method

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -20,5 +20,12 @@ class TestAccessNestedMap(unittest.TestCase):
         """ Test that the method returns what it is supposed to """
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
-if __name__ == '__main__':
-    unittest.main()
+    @parameterized.expand([
+        ({}, ("a",), 'a'),
+        ({"a": 1}, ("a", "b"), 'b')
+        ])
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        """ Test failed : with the wrong parameters """
+        with self.assertRaises(KeyError) as e:
+            access_nested_map(nested_map, path)
+        self.assertEqual(f"KeyError('{expected}')", repr(e.exception))
